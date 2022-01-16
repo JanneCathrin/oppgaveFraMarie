0865535db972561f890b7f4f3c3b35ba67fe30fc
JanneCathrin <77385680+JanneCathrin@users.noreply.github.com>
2022-01-17 00:15:03 +0100

    Add files via upload

---
+++ OppgaveFraMarie/Lærer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OppgaveFraMarie
+{
+    public class Lærer
+    {
+        public string _navn;
+        public string _fag;
+        public int _age;
+       
+
+
+        public Lærer(string name, int age, string fag)
+        {
+            _navn = name;
+            _fag = fag;
+            _age = age;
+        
+        }
+      
+
+        public void Bursdag()
+        {
+            _age += 1;
+            
+        }
+
+     public void Teach()
+        {
+           Console.WriteLine("Velkommen til " + _fag + " jeg heter " + _navn + " og er " + _age);
+        } 
+    }
+}
---
+++ OppgaveFraMarie/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace OppgaveFraMarie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Person1 = new Lærer("Janne", 24 , "Naturfag" );
+            Person1.Bursdag();
+            Person1.Teach();
+
+            var Person2 = new Lærer("Erlend", 25, "English");
+            Person2.Bursdag();
+            Person2.Teach();
+
+            var Person3 = new Lærer("Leon", 3, "Norsk" );
+            Person3.Bursdag();
+            Person3.Teach();
+
+        }
+    }
+}
+
+
